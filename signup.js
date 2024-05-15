266dcc46c93afccba23bd5f6f6110a8cd7604e0c
Raju Kumar <66488405+rajukumar7@users.noreply.github.com>
2024-05-15 17:45:41 +0530

    first commit

---
+++ signup.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("signup-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Simulate form submission (replace with actual logic to send data)
+  alert("Your information has been submitted!");
+});
